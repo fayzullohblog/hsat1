257f6f0d93ea572e8fc0b73012e696624ce8b054
fayzullohblog <fayzulloh00010003@gmail.com>
2023-10-11 12:32:01 +0500

    first commit

---
+++ full_hsat/accountapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .manager import MyUserManager
+# Create your models here.
+
+
+class MyUser(AbstractUser):
+    phone_number=models.CharField(max_length=30)
+    user_number_litter=models.CharField(max_length=40,unique=True,blank=True,null=True)
+
+
+    objects = MyUserManager()
+
+    EMAIL_FIELD = "phone_number"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["phone_number"]
+
+    def __str__(self) -> str:
+        return self.first_name
+
