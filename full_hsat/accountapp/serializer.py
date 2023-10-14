792dc6dd65adaf1bc15ed7300d187063e4dc3d35
fayzullohblog <fayzulloh00010003@gmail.com>
2023-10-14 17:32:32 +0500

    s

--- full_hsat/accountapp/serializer.py
+++ full_hsat/accountapp/serializer.py
@@ -4,12 +4,12 @@ from rest_framework_simplejwt.tokens import Token
 
 
 class MyTokenObtainPairSerializer(serializers.TokenObtainPairSerializer):
+    
     @classmethod
     def get_token(cls, user) -> Token:
-        print('---------------------------1')
         token=super().get_token(user)
         token['username']=user.username
-        print('-----2',token)
+       
         
         return token
         
--- full_hsat/accountapp/views.py
+++ full_hsat/accountapp/views.py
@@ -1,19 +1,12 @@
-from rest_framework import viewsets
-from django.shortcuts import render
 from .serializer import MyTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 # Create your views here.
 
 class MyTokenObtainPairView(TokenObtainPairView):
-    print('------0000')
     serializer_class=MyTokenObtainPairSerializer
 
-# @api_view(['POST'])
-# def Mytoken(reqyest):
-#     mytoken={'toke':'fayzulloh'}
 
-#     return Response(data=mytoken)
+
+
+
 
